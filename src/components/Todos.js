33374dbdc17853418f90019caa5ff6719169cead
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-28 01:51:34 -0800

    Initial commit

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
   render() {
     console.log('this.props.todos', this.props.todos);
 
-    return this.props.todos.map((todo) => <TodoItem todo={todo} />);
+    return this.props.todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} />
+    ));
   }
 }
+
+// PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired,
+};
 
 export default Todos;
