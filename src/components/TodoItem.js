abc0ac1081f05b8b4faee4cb4cd63ed0530a100e
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-28 02:38:17 -0800

    Initial commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
+  getStyle = () => {
+    if (this.props.todocompleted) {
+      return {
+        textDecoration: 'line-through',
+      };
+    } else {
+      return {
+        textDecoration: 'none',
+      };
+    }
+  };
+
   render() {
     return (
-      <div>
+      <div style={this.getStyle}>
         <p>{this.props.todo.title}</p>
       </div>
     );
   }
 }
 
+// PropTypes
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
 };
