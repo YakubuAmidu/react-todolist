ce6e864399cafd1beced054e620ade65e5d1e69d
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-28 06:04:07 -0800

    Initial commit

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 function Header() {
   return (
-    <header>
+    <header style={headerStyle}>
       <h1>TodoList</h1>
     </header>
   );
 }
+
+const headerStyle = {
+  background: '#333',
+  color: '#fff',
+  padding: '10px',
+};
 
 export default Header;
