085868c0d6dbdbd6e77b5a007d4e17db03c50f47
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-28 06:25:20 -0800

    Initial commit

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 class AddTodo extends Component {
   render() {
     return (
-      <form style={{ display: flex }}>
-        <input type='text' name='title' placeholder='Add todo...' />
-        <input type='submit' value='submit' style={{ flex: 1 }} />
+      <form style={{ display: 'flex' }}>
+        <input
+          type='text'
+          name='title'
+          style={{ flex: '10', padding: '5px' }}
+          placeholder='Add todo...'
+        />
+        <input type='submit' value='submit' style={{ flex: '1' }} />
       </form>
     );
   }
