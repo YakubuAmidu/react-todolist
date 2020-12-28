a9b6494b1b221a9400cacbf237871f8e5ddc3f43
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-28 00:57:14 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import Todos from './components/Todos';
 
-const App = () => {
-  return <div>App</div>;
-};
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take the trash out',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false,
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Todos />
+      </div>
+    );
+  }
+}
 
 export default App;
